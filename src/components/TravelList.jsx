acdbfebba3d327d5b92295cd00e24ad338d221ed
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-02-05 22:27:06 +0100

    Implement TravelList

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,55 +1,34 @@
 import { useState } from "react";
 import travelPlansData from "../assets/travel-plans.json";
-
+import TravelPlanCard from "./TravelPlanCard";
 
 function TravelList (){
 
- const[data, setData]= useState(travelPlansData);
-
-const labels =(totalCost, allInclusive) => {
+    const [travelPlan, setTravelPlan] =useState(travelPlansData);
     return(
-        <>
-        {totalCost <= 350 && <span className="greatDeal"> Great Deal</span>}
-        {totalCost >=1500 && <span className="premium"> Premium</span>}
-        {allInclusive === true && <span className="inclusive">All Inclusive</span> }
+        <div className="list">
+            {travelPlan.map((elmObj) => {
+                return (
+               <div key= {elmObj.id}>
+
+            <TravelPlanCard plan={elmObj} />
+           
+            
+
+
+               </div>
+
+                );
+            })}
+               
         
         
-        </>
-    )
-}
- const deleteList = (travelId) => {
-
-    const newList = data.filter((travel) => {
-        console.log(travel.id !== travelId);
-        return travel.id !== travelId;
-    });
-    setData(newList);
- }
-
-    return(
-<>
-
-
-{data.map((travelObj) =>{
-    return(
-        <div key = {travelObj.id} className= "vacation">
-            <img className= 'travel'src= {travelObj.image} alt="pic" />
-
-            <div className="content">     
-              <h3> {travelObj.destination} ({travelObj.days} Days)</h3>
-              <p> {travelObj.description} </p>
-              <p> <b> Price:</b> {travelObj.totalCost} € </p>
-              {labels(travelObj.totalCost,travelObj.allInclusive)}
-              <p > <button className="btn" onClick={() => {deleteList(travelObj.id)}}> Delete </button> </p>
-             </div>
-         
+        
         </div>
     )
-})}
-</>
-
-
-    )
 }
+
+    
+
 
 export default TravelList;
